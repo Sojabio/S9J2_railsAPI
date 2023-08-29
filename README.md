bec9a23c94136cf6ff77c5920bd1d5f731f3ad87
soja <solene.jabaud@gmail.com>
2023-08-29 12:33:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+- [x]  lier les users aux articles en base, puis change le seed en fonction
+- [x]  Rends le signup et signin possibles
+- [x]  Fais en sorte que la création et édition d'un article ne soient accessibles qu'aux utilisateurs connectés
+- [x]  L'auteur d'un article ? La personne qui s'est authentifiée
+- [x]  Fais en sorte que les fonctionnalités de destroy et d'edit/update ne soient accessibles qu'aux propriétaires des articles
---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Article.destroy_all
+User.destroy_all
+
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: "123456")
+end
+
+10.times do |id|
+  Article.create!(
+      title: Faker::Book.title,
+      content: Faker::Quotes::Shakespeare.king_richard_iii_quote,
+      user_id: rand(User.first.id..User.last.id)
+  )
+end
